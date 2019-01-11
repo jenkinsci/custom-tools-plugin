a755d3b30b15c81e4f930e1e87ede0481eabfe2d
Alex Taylor <ataylor@cloudbees.com>
2019-01-11 13:44:32 -0500

    Added a test and additional vars
    Added a test to properly prove my fix worked and also made sure that the additional variables string is pushed to ENV variables globally as well

--- src/main/java/com/cloudbees/jenkins/plugins/customtools/CustomToolEnvironmentContributor.java
+++ src/main/java/com/cloudbees/jenkins/plugins/customtools/CustomToolEnvironmentContributor.java
@@ -1,15 +1,23 @@
 package com.cloudbees.jenkins.plugins.customtools;
 
-import com.cloudbees.jenkins.plugins.customtools.CustomToolInstallWrapper.DescriptorImpl;
-import com.synopsys.arc.jenkinsci.plugins.customtools.CustomToolsLogger;
+
+import com.synopsys.arc.jenkinsci.plugins.customtools.CustomToolException;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.model.EnvironmentContributor;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tools.ToolInstallation;
+import hudson.Util;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
+import java.io.StringReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 
 
 import java.io.IOException;
@@ -21,12 +29,39 @@ public class CustomToolEnvironmentContributor extends EnvironmentContributor {
     @Override
     public void buildEnvironmentFor(Run r, EnvVars envs, TaskListener listener)
         throws IOException, InterruptedException {
+        Map<String, String> result = new LinkedHashMap<String, String>();
+        Properties properties = new Properties();
+
         CustomTool.DescriptorImpl tools = ToolInstallation.all().get(CustomTool.DescriptorImpl.class);
         for (CustomTool tool : tools.getInstallations()) {
             if (tool.getHome() != null) {
                 envs.put(tool.getName().toUpperCase(Locale.ENGLISH) +"_HOME", String.valueOf(tool.getHome()));
             }
+            if (tool.hasAdditionalVariables()){
+                String additionalVars = tool.getAdditionalVariables();
+                String escapedAdditionalVars = additionalVars;
+                escapedAdditionalVars = escapedAdditionalVars.replaceAll("(?<=[^\\\\])\\\\(?=[^n])(?=[^\\\\])(?=[^\n])", "\\\\\\\\");
+                StringReader stringReader = new StringReader(escapedAdditionalVars);
+                try {
+                    properties.load(stringReader);
+                } catch (IOException ioe) {
+                    throw new CustomToolException("Problem occurs on loading additional string content", ioe);
+                } finally {
+                    stringReader.close();
+                }
+                for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+                    envs.put(processElement(entry.getKey(), envs), processElement(entry.getValue(), envs));
+                }
+            }
         }
         super.buildEnvironmentFor(r, envs, listener);
+    }
+    @CheckForNull
+    private String processElement(@CheckForNull Object prop, @Nonnull Map<String, String> currentEnvVars) {
+        String macroProcessedElement = Util.replaceMacro(String.valueOf(prop), currentEnvVars);
+        if (macroProcessedElement == null) {
+            return null;
+        }
+        return macroProcessedElement.trim();
     }
 }
