e3efacf42683b38e552383b356bdb6f8ec5e2bc9
Alex Taylor <ataylor@cloudbees.com>
2019-03-07 13:57:19 -0500

    Added a check to see if the properties are configured
    Added a check to make sure that tools are installed and configured before loading the ENV variables as asked by Oleg

--- src/main/java/com/cloudbees/jenkins/plugins/customtools/CustomToolEnvironmentContributor.java
+++ src/main/java/com/cloudbees/jenkins/plugins/customtools/CustomToolEnvironmentContributor.java
@@ -9,18 +9,17 @@ import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tools.ToolInstallation;
 import hudson.Util;
+import hudson.tools.ToolProperty;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import java.io.StringReader;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 
 
 import java.io.IOException;
-import java.util.Locale;
 
 @Extension
 @Restricted(NoExternalUse.class)
@@ -28,36 +27,43 @@ public class CustomToolEnvironmentContributor extends EnvironmentContributor {
     @Override
     public void buildEnvironmentFor(Run r, EnvVars envs, TaskListener listener)
         throws IOException, InterruptedException {
-        Properties properties = new Properties();
+        //Checking if the tool propeties are actually configured so this only injects when there are custom tools configured
+        List<ToolProperty<ToolInstallation>> toolProperties = new ArrayList<ToolProperty<ToolInstallation>>();
+        if (toolProperties != null) {
+            Properties properties = new Properties();
 
-        CustomTool.DescriptorImpl tools = ToolInstallation.all().get(CustomTool.DescriptorImpl.class);
-        for (CustomTool tool : tools.getInstallations()) {
-            if (tool.getHome() != null) {
-                envs.put(tool.getName().toUpperCase(Locale.ENGLISH) +"_HOME", String.valueOf(tool.getHome()));
-            }
-            if (tool.hasAdditionalVariables()){
+            //Grabbing all the custom tool items
+            CustomTool.DescriptorImpl tools = ToolInstallation.all().get(CustomTool.DescriptorImpl.class);
+            for (CustomTool tool : tools.getInstallations()) {
+                //injecting the home for the tool
+                if (tool.getHome() != null) {
+                    envs.put(tool.getName().toUpperCase(Locale.ENGLISH) +"_HOME", String.valueOf(tool.getHome()));
+                }
+                //injecting the string of other defined variables
+                if (tool.hasAdditionalVariables()){
 
-                String additionalVariables = tool.getAdditionalVariables();
-                if (additionalVariables != null) {
-                    //Regex to escape any single \ characters which are not before \ or n. This is puilled from ENVinject plugin
-                    additionalVariables = additionalVariables.replaceAll("(?<=[^\\\\])\\\\(?=[^n])(?=[^\\\\])(?=[^\n])", "\\\\\\\\");
-                    StringReader stringReader = new StringReader(additionalVariables);
-                    //TO DO: Eventually update this to be try with resources but this plugin is compiled against old versions
-                    //of Jenkins so we can not guarantee Java 7
-                    try {
-                        properties.load(stringReader);
-                    } catch (IOException ioe) {
-                        throw new CustomToolException("Problem occurs on loading additional string content", ioe);
-                    } finally {
-                        stringReader.close();
-                    }
-                    for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-                        envs.put(processElement(entry.getKey(), envs), processElement(entry.getValue(), envs));
+                    String additionalVariables = tool.getAdditionalVariables();
+                    if (additionalVariables != null) {
+                        //Regex to escape any single \ characters which are not before \ or n. This is puilled from ENVinject plugin
+                        additionalVariables = additionalVariables.replaceAll("(?<=[^\\\\])\\\\(?=[^n])(?=[^\\\\])(?=[^\n])", "\\\\\\\\");
+                        StringReader stringReader = new StringReader(additionalVariables);
+                        //TO DO: Eventually update this to be try with resources but this plugin is compiled against old versions
+                        //of Jenkins so we can not guarantee Java 7
+                        try {
+                            properties.load(stringReader);
+                        } catch (IOException ioe) {
+                            throw new CustomToolException("Problem occurs on loading additional string content", ioe);
+                        } finally {
+                            stringReader.close();
+                        }
+                        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+                            envs.put(processElement(entry.getKey(), envs), processElement(entry.getValue(), envs));
+                        }
                     }
                 }
             }
+            super.buildEnvironmentFor(r, envs, listener);
         }
-        super.buildEnvironmentFor(r, envs, listener);
     }
     @CheckForNull
     private String processElement(@CheckForNull Object prop, @Nonnull Map<String, String> currentEnvVars) {
